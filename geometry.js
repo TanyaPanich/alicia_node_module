9db5fdb94a43bcdcc9380deffc427525b38cd1c1
Tanya <mellene@gmail.com>
2018-03-19 11:40:34 -0600

    dependecies stuff

--- geometry.js
+++ geometry.js
@@ -1,7 +1,8 @@
 //const volume = require('./volumes')
 //we don't want to repeat "volume", so we use OBJECT DESTRUCTION!
 const {sphereVolume, torusVolume} = require('./volumes')
-console.log(torusVolume(4, 3))
-console.log(sphereVolume(4, 3))
+const chalk = require('chalk')
+console.log(chalk.yellow(torusVolume(4, 3)))
+console.log(chalk.bgRed(chalk.black(sphereVolume(4, 3))))
 
 //console.log(volume(4, 3))
