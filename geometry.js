d212f44f0437718ac298102b0f4108f1e160f540
Tanya <mellene@gmail.com>
2018-03-19 11:11:31 -0600

    refactoring done

--- geometry.js
+++ geometry.js
@@ -1,6 +1,7 @@
-//Geometry stuff
-
-function torusVolume(majorRadius, minorRadius){
-  return (Math.PI *Math.pow(minorRadius, 2)) * (2 * Math.PI * majorRadius)
-}
+//const volume = require('./volumes')
+//we don't want to repeat "volume", so we use OBJECT DESTRUCTION!
+const {sphereVolume, torusVolume} = require('./volumes')
 console.log(torusVolume(4, 3))
+console.log(sphereVolume(4, 3))
+
+//console.log(volume(4, 3))
