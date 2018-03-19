fb39912042aaea7d58b599b1a9c3c92b899e70fc
Tanya <mellene@gmail.com>
2018-03-19 12:08:57 -0600

    added tests

--- geometry.js
+++ geometry.js
@@ -1,8 +1,8 @@
 //const volume = require('./volumes')
 //we don't want to repeat "volume", so we use OBJECT DESTRUCTION!
-const {sphereVolume, torusVolume} = require('./volumes')
+const {sphereVolume, torusVolume} = require('./src/volumes')
 const chalk = require('chalk')
 console.log(chalk.yellow(torusVolume(4, 3)))
-console.log(chalk.bgRed(chalk.black(sphereVolume(4, 3))))
+console.log(chalk.bgRed(chalk.black(sphereVolume(1))))
 
 //console.log(volume(4, 3))
---
+++ test/volume.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai')
+const { sphereVolume, torusVolume } = require('../src/volumes')
+
+describe('Canary test', function () {
+  it('expects true to be true', function () {
+    expect(true).to.equal(true)
+  })
+})
+
+describe('volume tests', function() {
+  it('calculates sphere volume correctly', function() {
+    expect(sphereVolume(1)).to.equal(4.1887902047863905)
+  })
+  it('calculates torus volume correctly', function() {
+    expect(torusVolume(4, 3)).to.equal(710.6115168784338)
+  })
+})
